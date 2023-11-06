468b9a41e914e6563876c300fcbb99118423da83
Anthony <anth06ny@gmail.com>
2023-11-06 09:33:10 +0100

    TP Permission avant

--- app/src/main/java/com/amonteiro/a23_09_fad_android/WeatherActivity.java
+++ app/src/main/java/com/amonteiro/a23_09_fad_android/WeatherActivity.java
@@ -7,6 +7,7 @@ import com.amonteiro.a23_09_fad_android.beans.WeatherBean;
 import com.amonteiro.a23_09_fad_android.databinding.ActivityWeatherBinding;
 import com.squareup.picasso.Picasso;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WeatherActivity extends AppCompatActivity {
@@ -20,37 +21,41 @@ public class WeatherActivity extends AppCompatActivity {
         binding = ActivityWeatherBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //Clic sur le bouton load
         binding.btLoad.setOnClickListener(v -> {
-
-            String city = binding.et.getText().toString();
-            binding.progressBar.setVisibility(View.VISIBLE);
-
-            //Tâche asynchrone
-            new Thread(() -> {
-                try {
-                    //Chercher les données
-                    WeatherBean weatherBean = RequestUtils.loadWeather(city);
-
-                    //Retourne sur le thread graphique
-                    runOnUiThread(() -> {
-                        binding.textView.setText("Il fait " + weatherBean.getMain().getTemp() + "° à " + weatherBean.getName() + " avec un vent de " + weatherBean.getWind().getSpeed() + " m/s");
-                        binding.progressBar.setVisibility(View.GONE);
-
-                        //Image
-                        if(!weatherBean.getWeather().isEmpty()) {
-                            Picasso.get().load("https://openweathermap.org/img/wn/" + weatherBean.getWeather().get(0).getIcon() + "@4x.png").into(binding.imageView);
-                        }
-                    });
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                    //Retourne sur le thread graphique
-                    runOnUiThread(() -> {
-                        binding.textView.setText("Une erreur est survenue : " + e.getMessage());
-                        binding.progressBar.setVisibility(View.GONE);
-                    });
-                }
-            }).start();
+            showWeather();
         });
+    }
+
+    public void showWeather(){
+        String city = binding.et.getText().toString();
+        binding.progressBar.setVisibility(View.VISIBLE);
+
+        //Tâche asynchrone
+        new Thread(() -> {
+            try {
+                //Chercher les données
+                WeatherBean weatherBean = RequestUtils.loadWeather(city);
+
+                //Retourne sur le thread graphique
+                runOnUiThread(() -> {
+                    binding.textView.setText("Il fait " + weatherBean.getMain().getTemp() + "° à " + weatherBean.getName() + " avec un vent de " + weatherBean.getWind().getSpeed() + " m/s");
+                    binding.progressBar.setVisibility(View.GONE);
+
+                    //Image
+                    if(!weatherBean.getWeather().isEmpty()) {
+                        Picasso.get().load("https://openweathermap.org/img/wn/" + weatherBean.getWeather().get(0).getIcon() + "@4x.png").into(binding.imageView);
+                    }
+                });
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                //Retourne sur le thread graphique
+                runOnUiThread(() -> {
+                    binding.textView.setText("Une erreur est survenue : " + e.getMessage());
+                    binding.progressBar.setVisibility(View.GONE);
+                });
+            }
+        }).start();
     }
 }
